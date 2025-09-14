5fef7b03fbee03d7eb42d91140e6457f51230c85
Artash <47955301+Artash-N@users.noreply.github.com>
2025-09-13 21:19:41 -0400

    Update camtest.py

--- camtest.py
+++ camtest.py
@@ -5,6 +5,7 @@ dispH=480
 flip=2
 
 camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=1280, height=720, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+cam= cv2.VideoCapture(camSet)
 
 while True:
     ret, frame = cam.read()
